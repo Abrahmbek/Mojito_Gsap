6fba57e30e96cc4a6ad7737927e235556f363c4f
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2025-07-10 22:20:28 +0900

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl text-indigo-300"> hello boss</h1>
+    </div>
+  );
+};
+
+export default App;
